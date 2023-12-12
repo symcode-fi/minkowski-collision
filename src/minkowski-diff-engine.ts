31bc6a407a6fc80a03673064764460eec71d7cad
Jari Hämäläinen <jari.hamalainen@symcode.fi>
2023-12-12 07:29:24 +0200

    feat: add line cast, point to body dist and single body collision check

--- src/minkowski-diff-engine.ts
+++ src/minkowski-diff-engine.ts
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2022, Symcode Oy
 import rfdc from 'rfdc';
+export type {MinDistResult, ReusablePointArray} from './minkowski-math.js';
 import {
   minkowskiSumConvex,
   ReusablePointArray,
@@ -42,10 +43,17 @@ export interface Body<T extends PolyData> extends PolyData {
   index: number;
   bb: BoundingBox;
   data: T;
+  castId: number;
 }
 
 export interface Collision<T extends PolyData> {
   a: Body<T>;
+  b: Body<T>;
+}
+
+export interface CastResult<T extends PolyData> {
+  // Ray / line collision point
+  pos: Vec2;
   b: Body<T>;
 }
 
@@ -87,6 +95,14 @@ function bbOverlap<T extends PolyData>(a: Body<T>, b: Body<T>): boolean {
     a.pos.y + a.bb.ymin > b.pos.y + b.bb.ymax
   );
 }
+function pointInBb<T extends PolyData>(a: Vec2, b: Body<T>): boolean {
+  return !(
+    a.x < b.pos.x + b.bb.xmin ||
+    a.x > b.pos.x + b.bb.xmax ||
+    a.y < b.pos.y + b.bb.ymin ||
+    a.y > b.pos.y + b.bb.ymax
+  );
+}
 
 /**
  * Collision detection with Minkowski difference algorith +
@@ -98,6 +114,11 @@ export default class MinkowskiDiffEngine<T extends PolyData> {
   public bodies: Body<T>[] = [];
   public collisions: Collision<T>[] = [];
   public collisionCount = 0;
+  public castResults: CastResult<T>[] = [];
+  public castResultCount = 0;
+  private castId = 0;
+  private castDistPArr: ReusablePointArray = {points: [], pCount: 0};
+  private minBbAxis = 0;
   // Only contains distances from smaller index bodies to larger index bodies.
   // Smaller index (a) defines the row, larger (b) defines the column.
   // Column = b - (a + 1); a === b and a < b are invalid cases.
@@ -151,18 +172,36 @@ export default class MinkowskiDiffEngine<T extends PolyData> {
       index,
       bb: polyBoundingBox(somePolyData),
       data: somePolyData,
+      castId: -1,
     };
   }
 
   updateData(data: T[]): void {
     let maxPoints = 0;
     this.bodies.splice(0);
+    this.minBbAxis = Number.POSITIVE_INFINITY;
 
     data.forEach((d, i) => {
       const body = this.createBody(d, i);
+      if (body.bb.xmax - body.bb.xmin < this.minBbAxis) {
+        this.minBbAxis = body.bb.xmax - body.bb.xmin;
+      }
+      if (body.bb.ymax - body.bb.ymin < this.minBbAxis) {
+        this.minBbAxis = body.bb.ymax - body.bb.ymin;
+      }
       this.bodies.push(body);
       maxPoints = Math.max(maxPoints, body.v.length);
+      if (i > this.castResults.length) {
+        this.castResults.push({
+          b: this.someBody,
+          pos: {x: 0, y: 0},
+        });
+      }
     });
+
+    while (this.castDistPArr.points.length < maxPoints) {
+      this.castDistPArr.points.push({refA: -1, refB: -1, x: 0, y: 0});
+    }
 
     // Minkowski sum max input point count = Math.max(n, m) = maxPoints
     while (this.p.points.length < maxPoints) {
@@ -275,52 +314,205 @@ export default class MinkowskiDiffEngine<T extends PolyData> {
     this.qn.iLowest = bb.iLowestN;
   }
 
-  checkCollisions(): void {
+  pointToBodyMinDist(pt: Vec2, body: Body<T>, ret: MinDistResult): MinDistResult {
+    // Put translated points to castDistPArr
+    body.v.forEach((v, i) => {
+      this.castDistPArr.points[i].x = v.x + body.pos.x;
+      this.castDistPArr.points[i].y = v.y + body.pos.y;
+    });
+    this.castDistPArr.pCount = body.v.length;
+    return pointPolyConvexMinDist(pt, this.castDistPArr, ret, this.epsilon);
+  }
+
+  lineCast(from: Vec2, to: Vec2) {
+    this.castId++;
+    this.castResultCount = 0;
+    // tv1 = coarse step
+    // tv2 = fine step = unit vector
+    this.tv1.x = to.x - from.x;
+    this.tv1.y = to.y - from.y;
+    const d = Math.sqrt(this.tv1.x * this.tv1.x + this.tv1.y * this.tv1.y);
+    const step = this.minBbAxis * 0.98;
+    let count = d / step + 1 + Number.EPSILON; // +1 because we check the starting point too!
+    this.tv2.x = this.tv1.x / d;
+    this.tv2.y = this.tv1.y / d;
+    this.tv1.x = this.tv2.x * step;
+    this.tv1.y = this.tv2.y * step;
+    // tv3 = coarse point
+    this.tv3.x = from.x;
+    this.tv3.y = from.y;
+
+    while (count > 0) {
+      // Check polys at tv3
+      for (let i = 0; i < this.bodies.length; i++) {
+        const b = this.bodies[i];
+        if (b.castId == this.castId) continue; // Already hit, ignore
+
+        // If hit, calc this first point hitting nothing, then move
+        // from there in small steps towards last coarse point and
+        // collect all hits.
+        if (pointInBb(this.tv3, b) && this.pointInBody(this.tv3, b)) {
+          // tv4 = binary search step vector
+          this.tv4.x = this.tv1.x;
+          this.tv4.y = this.tv1.y;
+          // tv5 = binary search check vector
+          this.tv5.x = this.tv3.x;
+          this.tv5.y = this.tv3.y;
+          let m = -2;
+
+          // Simplified calculus: we go finer until definitely under ½ unit vector.
+          let wasHit = true;
+          while (Math.abs(this.tv4.x) + Math.abs(this.tv4.y) > 0.5) {
+            let hit = false;
+            this.tv4.x /= m;
+            this.tv4.y /= m;
+            m = 2;
+            this.tv5.x += this.tv4.x;
+            this.tv5.y += this.tv4.y;
+            for (let j = 0; j < this.bodies.length; j++) {
+              const b2 = this.bodies[j];
+              if (b2.castId == this.castId) continue; // Already hit, ignore
+
+              hit =
+                hit ||
+                (pointInBb(this.tv5, b2) && this.pointInBody(this.tv5, b2));
+              // Shortcut: If there was a hit and we're still hitting, gotta keep moving.
+              if (wasHit && hit) break;
+              // Shortcut: If there wasn't a hit and we're now hitting, gotta turn around
+              if (!wasHit && hit) break;
+            }
+            // If hit status changed, turn around
+            if (wasHit != hit) {
+              m = -2;
+              wasHit = hit;
+            }
+          }
+
+          // Now go from tv5 towards tv3 using tv2 steps
+          const sx = Math.sign(this.tv3.x - this.tv5.x);
+          const sy = Math.sign(this.tv3.y - this.tv5.y);
+          while (
+            Math.sign(this.tv3.x - this.tv5.x) === sx &&
+            Math.sign(this.tv3.y - this.tv5.y) === sy
+          ) {
+            for (let j = 0; j < this.bodies.length; j++) {
+              const b2 = this.bodies[j];
+              if (b2.castId == this.castId) continue; // Already hit, ignore
+
+              if (pointInBb(this.tv5, b2) && this.pointInBody(this.tv5, b2)) {
+                b2.castId = this.castId;
+                const r = this.castResults[this.castResultCount++];
+                r.b = b2;
+                r.pos.x = this.tv5.x;
+                r.pos.y = this.tv5.y;
+              }
+            }
+            this.tv5.x += this.tv2.x;
+            this.tv5.y += this.tv2.y;
+          }
+        }
+      }
+
+      this.tv3.x += this.tv1.x;
+      this.tv3.y += this.tv1.y;
+      count--;
+    }
+  }
+
+  private pointInBody<T extends PolyData>(
+    pt: Vec2,
+    body: Body<T>,
+    epsilon = 1e-10,
+  ): boolean {
+    // Put translated points to castDistPArr and check
+    body.v.forEach((v, i) => {
+      this.castDistPArr.points[i].x = v.x + body.pos.x;
+      this.castDistPArr.points[i].y = v.y + body.pos.y;
+    });
+    this.castDistPArr.pCount = body.v.length;
+    return pointInPolyConvex(pt, this.castDistPArr, epsilon);
+  }
+
+  findBody(nodeId: string): Body<T> | undefined {
+    return this.bodies.find((b) => b.nodeId == nodeId);
+  }
+
+  checkBodyBodyCollision(ba: Body<T>, bb: Body<T>) {
+    this.collisionCount = 0;
+    this.msrCount = this.storeMsrs ? 0 : 1;
+    if (ba.index < bb.index) {
+      this.checkCollision(ba, bb, ba.index, bb.index);
+    } else {
+      this.checkCollision(bb, ba, bb.index, ba.index);
+    }
+  }
+
+  checkBodyCollision(ba: Body<T>, stopOnFirst = false) {
+    this.collisionCount = 0;
+    this.msrCount = this.storeMsrs ? 0 : 1;
+    for (let i = 0; i < this.bodies.length; i++) {
+      if (i == ba.index) continue;
+      const bb = this.bodies[i];
+      if (ba.index < bb.index) {
+        this.checkCollision(ba, bb, ba.index, bb.index);
+      } else {
+        this.checkCollision(bb, ba, bb.index, ba.index);
+      }
+      if (stopOnFirst && this.collisionCount) return;
+    }
+  }
+
+  checkCollisions(stopOnFirst = false): void {
     this.collisionCount = 0;
     this.msrCount = this.storeMsrs ? 0 : 1;
     for (let i = 0; i < this.bodies.length - 1; i++) {
       const ba = this.bodies[i];
       for (let j = i + 1; j < this.bodies.length; j++) {
         const bb = this.bodies[j];
-        // Bounding box check optimization
-        if (this.distCalc || bbOverlap(ba, bb)) {
-          // NOTE to self: making a sepate `updatePQnFn` where either updatePQn
-          //               or updatePQnReffed get assigned to, and using that
-          //               in here doesn't seem to help with performance. It may
-          //               make it even a little bit worse.
-          if (!this.distRefPoint) {
-            this.updatePQn(ba, bb);
-          } else {
-            this.updatePQnReffed(ba, bb);
-          }
-
-          const msr = this.msra[this.storeMsrs ? this.msrCount++ : 0];
-          minkowskiSumConvex(this.p, this.qn, msr, this.epsilon);
-          const collision = pointInPolyConvex(ZERO, msr, this.epsilon);
-          if (collision) {
-            const coll = this.collisions[this.collisionCount];
-            coll.a = ba;
-            coll.b = bb;
-            this.collisionCount++;
-          }
-
-          if (!this.distCalc) continue;
-
-          // Calculate and store distance from ba to bb, that's
-          // distance from their Minkowski sum to origin.
-          const bbd = this.distances[i][j - (i + 1)];
-          pointPolyConvexMinDist(ZERO, msr, this.mdr, this.epsilon);
-          bbd.d = this.mdr.d;
-          // this.mdr.minPoint is vector (origin -> Minkowski sum) which
-          // represents distance from bb to ba. Negate to get ba to bb.
-          bbd.dv.x = -this.mdr.minPoint.x;
-          bbd.dv.y = -this.mdr.minPoint.y;
-
-          if (!this.distRefPoint) continue;
-
-          this.updateRefpoints(ba, bb, msr, bbd, collision);
-        }
+        this.checkCollision(ba, bb, i, j);
+        if (stopOnFirst && this.collisionCount) return;
       }
+    }
+  }
+
+  private checkCollision(ba: Body<T>, bb: Body<T>, i: number, j: number) {
+    // Bounding box check optimization
+    if (this.distCalc || bbOverlap(ba, bb)) {
+      // NOTE to self: making a sepate `updatePQnFn` where either updatePQn
+      //               or updatePQnReffed get assigned to, and using that
+      //               in here doesn't seem to help with performance. It may
+      //               make it even a little bit worse.
+      if (!this.distRefPoint) {
+        this.updatePQn(ba, bb);
+      } else {
+        this.updatePQnReffed(ba, bb);
+      }
+
+      const msr = this.msra[this.storeMsrs ? this.msrCount++ : 0];
+      minkowskiSumConvex(this.p, this.qn, msr, this.epsilon);
+      const collision = pointInPolyConvex(ZERO, msr, this.epsilon);
+      if (collision) {
+        const coll = this.collisions[this.collisionCount];
+        coll.a = ba;
+        coll.b = bb;
+        this.collisionCount++;
+      }
+
+      if (!this.distCalc) return;
+
+      // Calculate and store distance from ba to bb, that's
+      // distance from their Minkowski sum to origin.
+      const bbd = this.distances[i][j - (i + 1)];
+      pointPolyConvexMinDist(ZERO, msr, this.mdr, this.epsilon);
+      bbd.d = this.mdr.d;
+      // this.mdr.minPoint is vector (origin -> Minkowski sum) which
+      // represents distance from bb to ba. Negate to get ba to bb.
+      bbd.dv.x = -this.mdr.minPoint.x;
+      bbd.dv.y = -this.mdr.minPoint.y;
+
+      if (!this.distRefPoint) return;
+
+      this.updateRefpoints(ba, bb, msr, bbd, collision);
     }
   }
 
